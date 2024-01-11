598491ca55f65e310b0c8bfdcc1e8f34b12c431c
Salio <alio.s@outlook.com>
2024-01-11 17:26:51 +0000

    sets up src files

---
+++ reactpractice/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+    
+}
+
+function Navbar() {
+    return (
+        <nav className="Navbar" style={styles}>
+            <h1>Hello</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
